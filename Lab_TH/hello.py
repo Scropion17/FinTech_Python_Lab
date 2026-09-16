ddd125422009645f8c34c26933d51deb6ced7cfa
phamhuyhoang0317261023 <0317261023@caothang.edu.vn>
2026-09-16 21:46:09 +0700

    Add files via upload

---
+++ Lab_TH/hello.py
@@ -0,0 +1,10 @@
+"""Bài 2: Script Hello FinTech đầu tiên."""
+
+# Mô tả: In thông báo khởi tạo hệ thống tài chính số
+print("==================================================")
+print("   CHAO_MUNG_ĐEN_VOI_HE_THONG_FINTECH_&_DIGITAL   ")
+print("==================================================")
+
+# In thông báo trạng thái
+print("Khoi_tao_moi_truong_lap_trinh_Python_thanh_cong.")
+print("San_sang_xu_ly_cac_giao_dich_tai_chinh_tu_đong!")
